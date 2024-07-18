0e853e38895a9c84a06b5c94c8ecd92f967907f2
Filipe Bila <TC_Bila@CMDO-SECCON-10.ESPCEX>
2024-07-18 16:53:54 -0300

    Update sismal.py

--- sismal.py
+++ sismal.py
@@ -1,1 +1,38 @@
-print('Teste atual')
+#Estrutura principal do programa de planejamento e execução da logística de transporte de material universitário
+
+
+#Importando as bibliotecas necessárias
+import pandas as pd
+
+
+#Listando as funções necessárias
+
+#Cadastrando os locais de destino
+def cadastro_omse():
+    candidatos = int(input("Quantidade de Candidatos: "))
+    salas = int(input("Quantidades de Salas/Setores: "))
+                     
+    print('ok')
+    return candidatos, salas
+
+
+#Cadastrando materiais
+def cadastro_material():
+    print('ok1')
+    
+#Calculando peso e volumes
+def peso_volumes(candidatos, salas):
+    print('ok2', candidatos, salas)
+    
+#Gerando guias de transporte
+def gera_guia():
+    print('ok3')
+    
+def main():
+    candidatos, salas = cadastro_omse()
+    cadastro_material()
+    peso_volumes(candidatos, salas)
+    gera_guia()
+    
+if __name__ == "__main__":
+    main()

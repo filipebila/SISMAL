7e8248190736150c71ae1907809faf6a6fbc05ff
Filipe Bila <TC_Bila@CMDO-SECCON-10.ESPCEX>
2024-07-22 15:45:06 -0300

    Update sismal.py

--- sismal.py
+++ sismal.py
@@ -3,36 +3,54 @@
 
 #Importando as bibliotecas necessárias
 import pandas as pd
-
+import csv
 
 #Listando as funções necessárias
-
-#Cadastrando os locais de destino
+    #Cadastrando os locais de destino
 def cadastro_omse():
-    candidatos = int(input("Quantidade de Candidatos: "))
-    salas = int(input("Quantidades de Salas/Setores: "))
-                     
-    print('ok')
-    return candidatos, salas
-
-
-#Cadastrando materiais
-def cadastro_material():
-    print('ok1')
+    omse = {}
+    omse['Número da OMSE'] = int(input('Número da OMSE: '))
+    omse['Organização Militar'] = input('Organização Militar: ')
+    omse['Quantidade de Candidatos'] = int(input('Quantidade de CAndidatos: '))
+    omse['Endereço da OM'] = input('Endereço da OM: ')
+    omse['Local de prova'] = input('Endereço do local de prova: ')
+    omse['Quantidade de setores de prova'] = int(input('Quantidade de setores: '))
     
-#Calculando peso e volumes
-def peso_volumes(candidatos, salas):
-    print('ok2', candidatos, salas)
+    omse['Quantidade de Setores ate 12'] = int(input('Quantidade de setores até 12: '))
+    omse['Quantidade de Setores ate 22'] = int(input('Quantidade de setores até 22: '))
+    omse['Quantidade de Setores ate 27'] = int(input('Quantidade de setores até 27: '))
+    omse['Quantidade de Setores ate 33'] = int(input('Quantidade de setores até 33: '))
+    omse['Quantidade de Setores ate 39'] = int(input('Quantidade de setores até 39: '))
+    omse['Quantidade de Setores ate 45'] = int(input('Quantidade de setores até 45: '))
+    omse['Quantidade de Setores ate 99'] = int(input('Quantidade de setores até 99: '))      
     
-#Gerando guias de transporte
-def gera_guia():
-    print('ok3')
+    arquivo_csv = 'omse.csv'
+    
+    with open(arquivo_csv, mode='a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=omse.keys())
+        writer.writeheader()
+        writer.writerow(omse)
+          
+    print('Dados salvos')
+    
+
+    #Cadastrando materiais
+#def cadastro_material():
+    #print('ok1')
+    
+    #Calculando peso e volumes
+#def peso_volumes(candidatos, salas):
+    #print('ok2', candidatos, salas)
+    
+    #Gerando guias de transporte
+#def gera_guia():
+    #print('ok3')
     
 def main():
-    candidatos, salas = cadastro_omse()
-    cadastro_material()
-    peso_volumes(candidatos, salas)
-    gera_guia()
+    #candidatos, salas = cadastro_omse()
+    cadastro_omse()
+    #peso_volumes(candidatos, salas)
+    #gera_guia()
     
 if __name__ == "__main__":
     main()
